7385d80a4c4b8d806b9e7efa3a26b20851116cd8
Emerson Hsieh <emersonhsieh@berkeley.edu>
2023-02-26 06:49:56 -0500

    Calling Perlin.noise causes deployment error

--- packages/contracts/src/systems/IncrementSystem.sol
+++ packages/contracts/src/systems/IncrementSystem.sol
@@ -4,6 +4,7 @@ import { System, IWorld } from "solecs/System.sol";
 import { getAddressById } from "solecs/utils.sol";
 import { CounterComponent, ID as CounterComponentID } from "components/CounterComponent.sol";
 import { LibMath } from "libraries/LibMath.sol";
+import { Perlin } from "noise/Perlin.sol";
 
 uint256 constant ID = uint256(keccak256("system.Increment"));
 
@@ -13,6 +14,8 @@ contract IncrementSystem is System {
   function execute(bytes memory arguments) public returns (bytes memory) {
     uint256 entity = abi.decode(arguments, (uint256));
     CounterComponent c = CounterComponent(getAddressById(components, CounterComponentID));
+
+    int128 a = Perlin.noise2d(0, 0, 0, 0);
     LibMath.increment(c, entity);
   }
 
